ce48051e85390499bc82ad2eb1ddabe4b0eb5a90
kevin <0010216@uca.edu.sv>
2018-04-07 01:48:48 -0600

    Some comments added

--- app/src/main/java/com/velasquez/calculadoraderedes/MainActivity.java
+++ app/src/main/java/com/velasquez/calculadoraderedes/MainActivity.java
@@ -9,6 +9,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
     EditText edit_ip, edit_mask;
     TextView text_netmask, text_network, text_broadcast, text_hosts;
@@ -28,7 +38,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Se infla el layout
         setContentView(R.layout.activity_main);
+        //Se obtienen referencias a todos los objetos ya creados en el heap
         edit_ip = findViewById(R.id.edit_ip);
         edit_mask = findViewById(R.id.edit_mask);
         btn_calcular = findViewById(R.id.btn_calcular);
@@ -50,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
                 getNetwork();
                 getBroadcast();
                 getHostPart();
-                text_hosts.setText("Hosts: " + (int)(Math.pow(2, 32-Integer.parseInt(edit_mask.getText().toString()))-2));
+                text_hosts.setText("Hosts: " + (int)Math.pow(2, 32-Integer.parseInt(edit_mask.getText().toString())-2));
             }
             catch (IllegalArgumentException e) {
                 Toast toast = Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT);
@@ -76,6 +88,9 @@ public class MainActivity extends AppCompatActivity {
 
     void getNetMask() {
         String temp = edit_mask.getText().toString().trim();
+
+        //Antes de calcular la máscara, se hacen las validaciones necesarias
+
         if (temp.isEmpty())
             throw new IllegalArgumentException("Campo de máscara vacío");
         else if (Integer.parseInt(temp) < 1 || Integer.parseInt(temp) > 32)
@@ -86,6 +101,9 @@ public class MainActivity extends AppCompatActivity {
             mask[1] = prefix >> 16 & 0xff;
             mask[2] = prefix >> 8 & 0xff;
             mask[3] = prefix & 0xff;
+
+            //Ahora vamos a calcular la wildcard mask, que es exactamente el reverso, bit por bit, de la máscara
+
             wildcard[0] = (~mask[0] << 24) >>> 24;
             wildcard[1] = (~mask[1] << 24) >>> 24;
             wildcard[2] = (~mask[2] << 24) >>> 24;
@@ -94,12 +112,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //La parte del host se obtiene haciendo un bitwise-AND entre la negación bit por bit de la máscara y la dirección
     void getHostPart() {
         host =  (~mask[0] & mip[0]) + "." + (mip[1] & ~mask[1]) + "." + (mip[2] & ~mask[2])
                 + "." + (mip[3] & ~mask[3]);
         text_hostpart.setText("Host part:"+" "+host);
     }
 
+    //La parte de la red se obtiene haciendo un bitwise-AND entre la máscara y la dirección de la IP
     void getNetwork() {
         network = (mask[0] & mip[0]) + "." + (mip[1] & mask[1]) + "." + (mip[2] & mask[2])
                 + "." + (mip[3] & mask[3]);
@@ -107,6 +127,7 @@ public class MainActivity extends AppCompatActivity {
         text_netpart.setText("Red:"+" "+ network);
     }
 
+    //El broadcast se obtiene con un bitwise-OR entre la IP y el reverso de la máscara, que es la wildcard mask y ya calculamos
     void getBroadcast() {
         broadcast = (wildcard[0] | mip[0]) + "." + (mip[1] | wildcard[1]) + "." + (mip[2] | wildcard[2])
                 + "." + (mip[3] | wildcard[3]);
@@ -116,3 +137,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+
